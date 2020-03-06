47c2276d2aad93e8f8cdadccce9cda9aaea552eb
R3NDL3 <evilpigproductions@gmail.com>
2020-03-05 20:05:51 -0600

    Initial

---
+++ Assets/Tuna.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tuna : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Milk();
+	}
+	void Milk (){
+		Debug.Log("I am a beautiful boy.");
+	}
+}
