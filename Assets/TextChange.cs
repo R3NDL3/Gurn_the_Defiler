cf7c582708fdc9272c7cb601bf871110e247baa8
R3NDL3 <evilpigproductions@gmail.com>
2020-03-05 22:14:35 -0600

    StartButton

---
+++ Assets/TextChange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextChange : MonoBehaviour
+{
+    public Text story;
+    // Start is called before the first frame update
+    void Start()
+    {
+        story.text = "You are the hero of Canterburg, a town far away from the castle of Gurn.It is up to you to slay Gurn and his minions of darkness to bring Canterburg back into the graces of the gods.";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Change()
+    {
+        story.text = "Choose your class";
+    }
+}
